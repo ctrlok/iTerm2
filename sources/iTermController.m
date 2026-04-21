ctrlok/iTerm2

commit 423f48bb1a135d9f938a15e794e6ebcdb6b74aff

Add the ability to save arrangements with contents to a file.

sources/iTermController.m
@@ -39,6 +39,7 @@
 #import "iTermPresentationController.h"
 #import "iTermProfileModelJournal.h"
 #import "iTermRestorableStateController.h"
+#import "iTermSavePanel.h"
 #import "iTermSessionFactory.h"
 #import "iTermSessionLauncher.h"
 #import "iTermWebSocketCookieJar.h"
@@ -491,14 +492,15 @@ replaceInitialDirectoryForSessionWithGUID:(NSString *)guid
 }
 
 - (void)saveWindowArrangement:(BOOL)allWindows {
-    NSString *name = [WindowArrangements nameForNewArrangement];
+    BOOL includeContents = NO;
+    NSString *name = [WindowArrangements selectNameAndWhetherToIncludeContents:&includeContents];
     if (!name) {
         return;
     }
-    [self saveWindowArrangementForAllWindows:allWindows name:name];
+    [self saveWindowArrangementForAllWindows:allWindows name:name includeContents:includeContents];
 }
 
-- (void)saveWindowArrangementForAllWindows:(BOOL)allWindows name:(NSString *)name {
+- (void)saveWindowArrangementForAllWindows:(BOOL)allWindows name:(NSString *)name includeContents:(BOOL)includeContents {
     if (allWindows) {
         NSArray<PseudoTerminal *> *sortedTerminalWindows = [_terminalWindows sortedArrayUsingComparator:^NSComparisonResult(PseudoTerminal *lhs, PseudoTerminal *rhs) {
             if (lhs.number == rhs.number) {
@@ -509,29 +511,37 @@ replaceInitialDirectoryForSessionWithGUID:(NSString *)guid
         }];
         NSMutableArray *terminalArrangements = [NSMutableArray arrayWithCapacity:[sortedTerminalWindows count]];
         for (PseudoTerminal *terminal in sortedTerminalWindows) {
-            NSDictionary *arrangement = [terminal arrangement];
+            NSDictionary *arrangement = [terminal arrangementExcludingTmuxTabs:YES includingContents:includeContents];
             if (arrangement) {
                 [terminalArrangements addObject:arrangement];
             }
         }
-        [WindowArrangements setArrangement:terminalArrangements withName:name];
+        if (includeContents) {
+            [terminalArrangements writeToFile:name atomically:NO];
+        } else {
+            [WindowArrangements setArrangement:terminalArrangements withName:name];
+        }
     } else {
         PseudoTerminal *currentTerminal = [self currentTerminal];
         if (!currentTerminal) {
             return;
         }
-        [self saveWindowArrangementForWindow:currentTerminal name:name];
+        [self saveWindowArrangementForWindow:currentTerminal name:name includeContents:includeContents];
     }
 }
 
-- (void)saveWindowArrangementForWindow:(PseudoTerminal *)currentTerminal name:(NSString *)name {
+- (void)saveWindowArrangementForWindow:(PseudoTerminal *)currentTerminal name:(NSString *)name includeContents:(BOOL)includeContents {
     NSMutableArray *terminalArrangements = [NSMutableArray arrayWithCapacity:[_terminalWindows count]];
     NSDictionary *arrangement = [currentTerminal arrangement];
     if (arrangement) {
         [terminalArrangements addObject:arrangement];
     }
     if (terminalArrangements.count) {
-        [WindowArrangements setArrangement:terminalArrangements withName:name];
+        if (includeContents) {
+            [terminalArrangements writeToFile:name atomically:NO];
+        } else {
+            [WindowArrangements setArrangement:terminalArrangements withName:name];
+        }
     }
 }
 
@@ -557,6 +567,16 @@ replaceInitialDirectoryForSessionWithGUID:(NSString *)guid
           [self addTerminalWindow:term];
         }
     }];
+}
+
+- (void)importWindowArrangementAtPath:(NSString *)path asTabsInTerminal:(PseudoTerminal *)term {
+    NSArray *arrangements = [NSArray arrayWithContentsOfFile:path];
+    if (!arrangements) {
+        return;
+    }
+    for (NSDictionary *terminalArrangement in arrangements) {
+        [self tryOpenArrangement:terminalArrangement named:path.lastPathComponent.stringByDeletingPathExtension asTabsInWindow:term];
+    }
 }
 
 - (BOOL)loadWindowArrangementWithName:(NSString *)theName asTabsInTerminal:(PseudoTerminal *)term {

Interfaces/MainMenu.xib
@@ -1557,6 +1557,12 @@ DQ
                                     <action selector="saveCurrentWindowAsArrangement:" target="201" id="FAg-xE-xg0"/>
                                 </connections>
                             </menuItem>
+                            <menuItem title="Load Arrangement from File…" identifier="Load Arrangement from File…" id="wNF-TH-Fz5">
+                                <modifierMask key="keyEquivalentModifierMask"/>
+                                <connections>
+                                    <action selector="importWindowArrangement:" target="201" id="2uC-bW-99w"/>
+                                </connections>
+                            </menuItem>
                             <menuItem title="Restore Window Arrangement" id="1280">
                                 <modifierMask key="keyEquivalentModifierMask"/>
                                 <menu key="submenu" title="Restore Window Arrangement" id="1281"/>

plists/beta-iTerm2.plist
@@ -31,6 +31,18 @@
                 <dict>
                         <key>CFBundleTypeExtensions</key>
                         <array>
+                                <string>iterm2arrangement</string>
+                        </array>
+                        <key>CFBundleTypeIconFile</key>
+                        <string>iTerm2.icns</string>
+                        <key>CFBundleTypeName</key>
+                        <string>iTerm2 Arrangement</string>
+                        <key>CFBundleTypeRole</key>
+                        <string>Editor</string>
+                </dict>
+                <dict>
+                        <key>CFBundleTypeExtensions</key>
+                        <array>
                                 <string>itermtab</string>
                         </array>
                         <key>CFBundleTypeIconFile</key>

plists/nightly-iTerm2.plist
@@ -31,6 +31,18 @@
                 <dict>
                         <key>CFBundleTypeExtensions</key>
                         <array>
+                                <string>iterm2arrangement</string>
+                        </array>
+                        <key>CFBundleTypeIconFile</key>
+                        <string>iTerm2.icns</string>
+                        <key>CFBundleTypeName</key>
+                        <string>iTerm2 Arrangement</string>
+                        <key>CFBundleTypeRole</key>
+                        <string>Editor</string>
+                </dict>
+                <dict>
+                        <key>CFBundleTypeExtensions</key>
+                        <array>
                                 <string>itermtab</string>
                         </array>
                         <key>CFBundleTypeIconFile</key>

plists/preview-iTerm2.plist
@@ -27,6 +27,18 @@
                 <dict>
                         <key>CFBundleTypeExtensions</key>
                         <array>
+                                <string>iterm2arrangement</string>
+                        </array>
+                        <key>CFBundleTypeIconFile</key>
+                        <string>iTerm2.icns</string>
+                        <key>CFBundleTypeName</key>
+                        <string>iTerm2 Arrangement</string>
+                        <key>CFBundleTypeRole</key>
+                        <string>Editor</string>
+                </dict>
+                <dict>
+                        <key>CFBundleTypeExtensions</key>
+                        <array>
                                 <string>itermtab</string>
                         </array>
                         <key>CFBundleTypeIconFile</key>

plists/release-iTerm2.plist
@@ -27,6 +27,18 @@
                 <dict>
                         <key>CFBundleTypeExtensions</key>
                         <array>
+                                <string>iterm2arrangement</string>
+                        </array>
+                        <key>CFBundleTypeIconFile</key>
+                        <string>iTerm2.icns</string>
+                        <key>CFBundleTypeName</key>
+                        <string>iTerm2 Arrangement</string>
+                        <key>CFBundleTypeRole</key>
+                        <string>Editor</string>
+                </dict>
+                <dict>
+                        <key>CFBundleTypeExtensions</key>
+                        <array>
                                 <string>itermtab</string>
                         </array>
                         <key>CFBundleTypeIconFile</key>

sources/VT100ScreenMark.m
@@ -239,13 +239,17 @@ static NSString *const kMarkOutputStart = @"Output Start";
     dict[kMarkCodeKey] = @(_code);
     dict[kMarkPromptDetectedByTrigger] = @(_promptDetectedByTrigger);
     dict[kMarkLineStyleKey] = @(_lineStyle);
-    dict[kMarkCommandKey] = _command ?: [NSNull null];
+    if (_command) {
+        dict[kMarkCommandKey] = _command;
+    }
     dict[kMarkStartDateKey] = @([self.startDate timeIntervalSinceReferenceDate]);
     if (_name) {
         dict[kMarkNameKey] = _name;
     }
     dict[kMarkEndDateKey] = @([self.endDate timeIntervalSinceReferenceDate]);
-    dict[kMarkSessionGuidKey] = self.sessionGuid ?: [NSNull null];
+    if (self.sessionGuid) {
+        dict[kMarkSessionGuidKey] = self.sessionGuid;
+    }
     dict[kMarkPromptRange] = [NSDictionary dictionaryWithGridAbsCoordRange:_promptRange];
     dict[kMarkPromptText] = [_promptText mapWithBlock:^id _Nullable(ScreenCharArray *sca) {
         return sca.dictionaryValue;

sources/WindowArrangements.h
@@ -34,6 +34,7 @@
                             identifier:(NSString *)identifier;
 
 + (NSString *)nameForNewArrangement;
++ (NSString *)selectNameAndWhetherToIncludeContents:(out BOOL *)includeContentsPtr;
 
 - (IBAction)setDefault:(id)sender;
 - (IBAction)deleteSelectedArrangement:(id)sender;

commit d43e5f49254e039822512b3a9bbaf84e9deab9e0

Fix Save Current Window to File with Contents… dropping contents
The single-window branch called -[PseudoTerminal arrangement], which is
a convenience that hard-codes includingContents:NO. The resulting file
could not restore scrollback or any other content-bearing state. Only
the all-windows branch passed includingContents correctly. Broken
since the feature was introduced in 423f48bb1 (Sep 2024).

sources/iTermController.m
@@ -533,7 +533,8 @@ replaceInitialDirectoryForSessionWithGUID:(NSString *)guid
 // Precondition: at least one of name,saveItem is nonnil.
 - (void)saveWindowArrangementForWindow:(PseudoTerminal *)currentTerminal name:(NSString *)name saveItem:(iTermSavePanelItem *)saveItem {
     NSMutableArray *terminalArrangements = [NSMutableArray arrayWithCapacity:[_terminalWindows count]];
-    NSDictionary *arrangement = [currentTerminal arrangement];
+    NSDictionary *arrangement = [currentTerminal arrangementExcludingTmuxTabs:YES
+                                                           includingContents:saveItem != nil];
     if (arrangement) {
         [terminalArrangements addObject:arrangement];
     }